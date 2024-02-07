f60023d39bba153af35056d53296a1024afbbb36
ishworpokhrel <65589785+Ishwor-git@users.noreply.github.com>
2024-02-07 17:48:29 +0545

    advance hello world program

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This repo contains simple experimental programs and projects that were built while learning c++ language.
 
-## 1.
+## 1. Bruteforce on "Hello World"
+
+unlike classic hello world program this is advance form of it. In this program we bruteforce each letter until it finds string "Hello World".
---
+++ helloWorld/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+using namespace std;
+
+int main(void)
+{
+    string message = "Hello World";
+    string characters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    string current_string = "";
+
+    for (int i = 0; i < message.length(); i++)
+    {
+        for (int j = 0; j < characters.length(); j++)
+        {
+            char prefix = characters[j];
+            system("clear");
+            cout << current_string + prefix << endl;
+
+            chrono::milliseconds delay(20);
+            this_thread::sleep_for(delay);
+            if (message[i] == prefix)
+            {
+                current_string += prefix;
+                break;
+            }
+        }
+    }
+}
