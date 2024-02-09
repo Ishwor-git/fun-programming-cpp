8e0f1d8bafc451029d33f6b67720325d687866aa
ishworpokhrel <65589785+Ishwor-git@users.noreply.github.com>
2024-02-09 21:17:17 +0545

    class for 2 and 3 ratios

---
+++ 3DGeometry/ratio.hpp
@@ -0,0 +1,54 @@
+#ifndef RATIO_HPP
+#define RATIO_HPP
+#include <iostream>
+using namespace std;
+
+class Ratio2
+{
+protected:
+    float m1, m2;
+
+public:
+    Ratio2(void);
+    Ratio2(float, float);
+    friend ostream &operator<<(ostream &, Ratio2 &);
+    float getM1() const { return m1; }
+    float getM2() const { return m2; }
+};
+class Ratio3
+{
+protected:
+    float a, b, c;
+
+public:
+    float getA() const { return a; }
+    float getB() const { return b; }
+    float getC() const { return c; }
+
+    Ratio3(void);
+    Ratio3(float, float, float);
+    friend ostream &operator<<(ostream &, Ratio3 &);
+};
+Ratio2::Ratio2() : m1(1), m2(1) {}
+
+Ratio2::Ratio2(float r1, float r2) : m1(r1), m2(r2) {}
+
+Ratio3::Ratio3() : a(1), b(1), c(1) {}
+
+Ratio3::Ratio3(float r1, float r2, float r3) : a(r1), b(r2), c(r3) {}
+
+ostream &operator<<(ostream &out, const Ratio2 &r)
+{
+    out << r.getM1() << " / " << r.getM2();
+    // out << r.m1 << " / " << r.m2;
+    return out;
+}
+
+ostream &operator<<(ostream &out, const Ratio3 &r)
+{
+    out << "( " << r.getA() << ", " << r.getB() << ", " << r.getC() << " )";
+    // out << "( " << r.a << ", " << r.b << ", " << r.c << " )";
+
+    return out;
+}
+#endif // RATIO_HPP
