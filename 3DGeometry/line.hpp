a348473c8c02a51a36ae143096a23187b05dbc5a
ishworpokhrel <65589785+Ishwor-git@users.noreply.github.com>
2024-02-10 21:10:04 +0545

    ratio formula computation

--- 3DGeometry/line.hpp
+++ 3DGeometry/line.hpp
@@ -15,6 +15,7 @@ public:
     Ratio3 dCosine(void);
     float length(void);
     friend float LAngle(Line &, Line &);
+    friend Position ratioFormula(Line &, Ratio2 &);
 };
 
 Line::Line(Position p1, Position p2) : initial(p1), final(p2) {}
@@ -22,9 +23,9 @@ Line::Line(Position p1, Position p2) : initial(p1), final(p2) {}
 Ratio3 Line ::dRatio()
 {
     Ratio3 dr;
-    float a = final.xAxis - initial.xAxis;
-    float b = final.yAxis - initial.yAxis;
-    float c = final.zAxis - initial.zAxis;
+    float a = final.x - initial.x;
+    float b = final.y - initial.y;
+    float c = final.z - initial.z;
     dr = {a, b, c};
     return dr;
 }
@@ -37,6 +38,8 @@ Ratio3 Line ::dCosine()
     return dc;
 }
 
+float Line ::length() { return distance(initial, final); }
+
 float LAngle(Line &l1, Line &l2)
 {
     float deg, divident, divisor;
@@ -48,4 +51,15 @@ float LAngle(Line &l1, Line &l2)
     deg = acos(divident / divisor);
     return deg;
 }
+
+Position ratioFormula(Line &l, Ratio2 &r)
+{
+    Position p;
+    float rSum = r.getM1() + r.getM2();
+    p.x = ((r.getM1() * l.final.x) + (r.getM2() * l.initial.x)) / rSum;
+    p.y = ((r.getM1() * l.final.y) + (r.getM2() * l.initial.y)) / rSum;
+    p.z = ((r.getM1() * l.final.z) + (r.getM2() * l.initial.z)) / rSum;
+    return p;
+}
+
 #endif // LINE_HPP
