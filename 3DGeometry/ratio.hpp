87df6a2fa05534564ef2411837470419bd3b80ea
ishworpokhrel <65589785+Ishwor-git@users.noreply.github.com>
2024-02-10 21:09:50 +0545

    updated op overloading

--- 3DGeometry/ratio.hpp
+++ 3DGeometry/ratio.hpp
@@ -1,7 +1,6 @@
 #ifndef RATIO_HPP
 #define RATIO_HPP
 #include <iostream>
-using namespace std;
 
 class Ratio2
 {
@@ -11,7 +10,8 @@ private:
 public:
     Ratio2(void);
     Ratio2(float, float);
-    friend ostream &operator<<(ostream &, Ratio2 &);
+    // ostream &operator<<(ostream &);
+    friend std::ostream &operator<<(std::ostream &, const Ratio2 &);
     float getM1() const { return m1; }
     float getM2() const { return m2; }
 };
@@ -27,7 +27,7 @@ public:
 
     Ratio3(void);
     Ratio3(float, float, float);
-    friend ostream &operator<<(ostream &, Ratio3 &);
+    friend std::ostream &operator<<(std::ostream &, const Ratio3 &);
 };
 Ratio2::Ratio2() : m1(1), m2(1) {}
 
@@ -37,14 +37,20 @@ Ratio3::Ratio3() : a(1), b(1), c(1) {}
 
 Ratio3::Ratio3(float r1, float r2, float r3) : a(r1), b(r2), c(r3) {}
 
-ostream &operator<<(ostream &out, const Ratio2 &r)
+// ostream &Ratio2::operator<<(ostream &out) const
+// {
+//     out << getM1() << " / " << getM2();
+//     // out << r.m1 << " / " << r.m2;
+//     return out;
+// }
+std::ostream &operator<<(std::ostream &out, const Ratio2 &r)
 {
     out << r.getM1() << " / " << r.getM2();
     // out << r.m1 << " / " << r.m2;
     return out;
 }
 
-ostream &operator<<(ostream &out, const Ratio3 &r)
+std::ostream &operator<<(std::ostream &out, const Ratio3 &r)
 {
     out << "( " << r.getA() << ", " << r.getB() << ", " << r.getC() << " )";
     // out << "( " << r.a << ", " << r.b << ", " << r.c << " )";
